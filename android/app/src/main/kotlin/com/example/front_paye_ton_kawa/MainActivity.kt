d019b0a20a01dc04f675cb1d08dd00a2bf902d6a
baptiste zeni <baptiste.zeni@bbox.fr>
2023-07-24 11:09:43 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/front_paye_ton_kawa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.front_paye_ton_kawa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/color_theme.dart
@@ -0,0 +1,71 @@
+import 'dart:ui';
+
+class AppColor {
+  //Couleur primaire
+  static Color primary100 = const Color(0xFF1f78bf);
+  static Color primary80 = const Color(0xFF4A92CB);
+  static Color primary60 = const Color(0xFF76acd8);
+  static Color primary40 = const Color(0xFFa2c8e5);
+  static Color primary20 = const Color(0xFFd1e3f1);
+  static Color primary10 = const Color(0xFFe7f0f8);
+  static Color primary05 = const Color(0xFFf2f7fb);
+
+  //Couleur secondaire
+  static Color secondary100 = const Color(0xFF70C31C);
+  static Color secondary80 = const Color(0xFF659c3c);
+  static Color secondary60 = const Color(0xFF8ab469);
+  static Color secondary40 = const Color(0xFFb1cd99);
+  static Color secondary20 = const Color(0xFFd7e5cb);
+  static Color secondary10 = const Color(0xFFeaf2e4);
+  static Color secondary05 = const Color(0xFFf4f8f1);
+
+  //Couleur tierce
+  static Color terciary100 = const Color(0xFF69709b);
+  static Color terciary80 = const Color(0xFF878cae);
+  static Color terciary60 = const Color(0xFFa4a8c2);
+  static Color terciary40 = const Color(0xFFc2c5d6);
+  static Color terciary20 = const Color(0xFFdfe1eb);
+  static Color terciary10 = const Color(0xFFefeff4);
+  static Color terciary05 = const Color(0xFFf6f7f9);
+
+  //Dark, Light & Shades
+  static Color dark = const Color(0xFF232323);
+  static Color shadesGrey = const Color(0xFF505050);
+  static Color lightGrey = const Color(0xFF757575);
+  static Color light = const Color(0xFFe7e5e2);
+  static Color white = const Color(0xFFFBFBFF);
+
+  //Background color
+  static Color bgGrey = const Color(0xFFe7e5e2);
+  static Color bgGrey75 = const Color(0xFFeaecee);
+  static Color bgGrey50 = const Color(0xFFf3f4f5);
+  static Color bgLight = const Color(0xFFfafafa);
+
+  //Success color
+  static Color success100 = const Color(0xFF16A34A);
+  static Color success80 = const Color(0xFF4eb36f);
+  static Color success60 = const Color(0xFF76c692);
+  static Color success40 = const Color(0xFFa2d8b6);
+  static Color success20 = const Color(0xFFcfecda);
+
+  //Warning color
+  static Color warning100 = const Color(0xFFf8843b);
+  static Color warning80 = const Color(0xFFf99d62);
+  static Color warning60 = const Color(0xFFfbb589);
+  static Color warning40 = const Color(0xFFfcceb1);
+  static Color warning20 = const Color(0xFFfee6d8);
+
+  //Error color
+  static Color error100 = const Color(0xFFDC2626);
+  static Color error80 = const Color(0xFFdf4e54);
+  static Color error60 = const Color(0xFFe77a7d);
+  static Color error40 = const Color(0xFFefa5a7);
+  static Color error20 = const Color(0xFFf6d2d3);
+
+  //Info color
+  static Color info100 = const Color(0xFF0284C7);
+  static Color info80 = const Color(0xFF399dce);
+  static Color info60 = const Color(0xFF68b5da);
+  static Color info40 = const Color(0xFF99cde7);
+  static Color info20 = const Color(0xFFcbe5f3);
+}
